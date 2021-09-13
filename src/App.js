3c8e1dff8d858f85478589450d8395534719333c
Breno Henrique <bhfs.2908@gmail.com>
2021-09-13 17:08:05 -0300

    feat:finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuImage from "./assets/images/menu.png";
 import LogoImage from "./assets/images/logo.png";
+import LampImage from "./assets/images/lamp.png";
+import LightImage from "./assets/images/light.png";
 import "./App.scss";
 
 const App = () => {
+  const [img, setImg] = useState(false);
+
+  const handleToogle = () => {
+    setImg(true);
+  };
+
   return (
     <div className="hero">
       <nav>
@@ -23,10 +31,34 @@ const App = () => {
             <a href="https://github.com/Breno44">Affordable</a>
           </li>
         </ul>
-        <button type="button">
+        <button type="button" onClick={handleToogle}>
           <span></span>
         </button>
       </nav>
+      <div class="lamp-container">
+        <img src={LampImage} className="lamp" alt="lamp" />
+        {img && <img src={LightImage} className="light" alt="light" />}
+      </div>
+
+      <div className="text-container">
+        <h1>
+          Latest
+          <br />
+          in lighting
+        </h1>
+        <p>
+          This is the first lamp from our company. we're making a huge collection of modern Lamps in all categories from
+          home use to office use.
+        </p>
+        <a href="https://github.com/Breno44">Check All Collections</a>
+        <div className="control">
+          <p>04</p>
+          <div className="line">
+            <span></span>
+          </div>
+          <p>05</p>
+        </div>
+      </div>
     </div>
   );
 };
